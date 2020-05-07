8767066ded9ff7bcb2652323d85e3216d2faed85
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-05-07 18:56:30 -0500

    styled sign in form and applied grid

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -12,8 +12,8 @@ class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
-      <PageTitle className='sign-in__page-title'  title='Login' />
-        <SignInForm onSubmit={this.onSubmit} className="sing-in__form" />
+        <PageTitle className='sign-in__page-title'  title='Login' />
+        <SignInForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     )
   }
